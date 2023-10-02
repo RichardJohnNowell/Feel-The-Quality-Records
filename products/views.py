1e063041916acd8f15bde8db1b474d0482b00ae0
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-10-02 11:50:25 +0000

    added context

--- products/views.py
+++ products/views.py
@@ -10,7 +10,4 @@ def all_products(request):
 
     context = {
         'products': products,
-        'search_term': query,
-        'current_categories': categories,
-        'current_sorting': current_sorting,
     }
