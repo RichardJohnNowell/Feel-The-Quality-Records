020dee62b8730525324c464b9a3b230bccb0c0b6
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-10-02 12:20:33 +0000

    added template variable products

--- ftqr/views.py
+++ ftqr/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+def all_products(request):
+    """ A view to return the products page """
+    return render(request, 'products/products.html', context)
