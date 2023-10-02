a363edf0e9243d80fef9561d26f6faef4b95acf1
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-10-02 11:49:32 +0000

    added function all_products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """ A view to return the index page """
+    return render(request, 'products/products.html', context)
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+    }
