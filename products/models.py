d4575943c97bfa7c6ef81c60ffa3950d6952196d
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-10-03 14:24:44 +0000

    added comment

--- products/models.py
+++ products/models.py
@@ -1,9 +1,17 @@
+"""
+products models.py
+"""
+
+
 from django.db import models
+
 
 class Category(models.Model):
 
+    
     class Meta:
         verbose_name_plural = 'Categories'
+
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
@@ -12,8 +20,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
     def get_friendly_name(self):
         return self.friendly_name
+
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True,
@@ -26,5 +36,9 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
+
     def __str__(self):
         return self.name
+
+
+    # end
