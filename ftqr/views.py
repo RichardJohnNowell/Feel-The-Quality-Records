3bde12f296444710b8797ff1185316438388a986
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-10-03 08:55:00 +0000

    commented out

--- ftqr/views.py
+++ ftqr/views.py
@@ -5,6 +5,9 @@ def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
 
+
+"""
 def all_products(request):
-    """ A view to return the products page """
+    A view to return the products page
     return render(request, 'products/products.html', context)
+"""
