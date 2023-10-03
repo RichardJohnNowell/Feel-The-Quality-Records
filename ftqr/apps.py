8c557d594ca8c036417121b35129304e8acd283c
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-10-03 14:03:10 +0000

    added comment

--- ftqr/apps.py
+++ ftqr/apps.py
@@ -1,6 +1,18 @@
+"""
+ftqr apps.py
+"""
+
+
 from django.apps import AppConfig
 
 
 class FtqrConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ftqr'
+
+
+# end
+
+
+
+
