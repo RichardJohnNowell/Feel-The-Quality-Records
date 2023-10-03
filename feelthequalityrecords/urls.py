cb2bdf4a6084e9708f0e93145c079b41119cf4df
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-10-03 13:56:04 +0000

    tidied urls.py

--- feelthequalityrecords/urls.py
+++ feelthequalityrecords/urls.py
@@ -1,11 +1,21 @@
+"""
+feelthequalityrecords urls
+"""
+
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+# url patterns tuple
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('', include('ftqr.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+#end
