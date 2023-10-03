c114b3eefe03e60fc36be8127bf2f74512bcc7f0
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-10-03 14:15:31 +0000

    added comment

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,11 @@
+"""
+products admin.py
+"""
+
+
 from django.contrib import admin
 from .models import Product, Category
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -9,7 +15,6 @@ class ProductAdmin(admin.ModelAdmin):
         'price',
         'image',
     )
-
     ordering = ('sku',)
 
 
