ae366ac7b4db5f9cc4127ae2dc8fd97d66520cc3
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-10-03 14:27:22 +0000

    added comment

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,16 @@
+"""
+products urls.py
+"""
+
+
 from django.urls import path
 from . import views
+
 
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
 ]
+
+
+# end
