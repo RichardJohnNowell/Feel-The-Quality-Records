350af7b74b0bf52dcc4e00c7075582b96329de0a
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-10-04 08:10:13 +0000

    added product detail module

--- products/views.py
+++ products/views.py
@@ -3,8 +3,9 @@ products views.py
 """
 
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product
+from django.contrib import messages
 
 
 def all_products(request):
@@ -16,6 +17,18 @@ def all_products(request):
     context = {
         'products': products,
     }
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
 
 
 # end
